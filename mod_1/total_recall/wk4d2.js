6f24b7ecaf5f81cdbcb18abe577b4c14d9198066
cburgee <caleb.burge@gmail.com>
2023-07-07 18:43:05 -0500

    completed wk4d2 homework

--- mod_1/total_recall/wk4d2.js
+++ mod_1/total_recall/wk4d2.js
@@ -135,3 +135,194 @@ if (newInt < 5) {
 } else {
   console.log("monkey")
 }
+
+// H-----------------------------------------------------------------------------------------------------------------
+const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "Per Scholas hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps",
+]
+
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],
+  [
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs",
+  ],
+  [
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans",
+  ],
+]
+
+console.log(`Kristyn is rocking that ${kristynsCloset[2]} today!`)
+kristynsCloset.splice(-1, 0, "raybans")
+kristynsCloset.splice(-3, 1, "stained knit hat")
+console.log(
+  `Thom is looking fierce in a ${thomsCloset[0][0]}, ${thomsCloset[1][0]} and ${thomsCloset[2][0]}`
+)
+thomsCloset[1].splice(-1, 1, "Footie Pajamas")
+console.log(thomsCloset)
+
+// FUNCTIONS!!!------------------------------------------------------------------------------------------------------------
+
+// B. printCool------------------------------------------------------------------------------------------------------------
+function printCool(name) {
+  if (typeof name === "string") {
+    console.log(`${name} is super coolio!`)
+  } else {
+    console.log("Argument must be of type 'string'.")
+  }
+}
+printCool("Caleb")
+
+// C. calculateCube------------------------------------------------------------------------------------------------------------
+function calculateCube(num) {
+  if (typeof num === "number") {
+    return Math.pow(num, 3)
+  } else {
+    console.log("Argument must be of type 'number'.")
+  }
+}
+console.log(calculateCube(5))
+
+// D. isVowel------------------------------------------------------------------------------------------------------------
+
+function isVowel(letter) {
+  const vowels = ["a", "e", "i", "o", "u"]
+  if (typeof letter === "string") {
+    for (i of vowels) {
+      if (i === letter) {
+        return true
+      }
+    }
+    return false
+  } else {
+    console.log("Argument must be of type 'string'.")
+  }
+}
+console.log(isVowel("z"))
+
+// E. getTwoLengths------------------------------------------------------------------------------------------------------------
+function getTwoLengths(str1, str2) {
+  return [str1.length, str2.length]
+}
+
+console.log(getTwoLengths("Hank", "Hippopottamus"))
+
+// F. getMultipleLengths------------------------------------------------------------------------------------------------------------
+function getMultipleLengths(arrOfStrings) {
+  let result = []
+  for (i of arrOfStrings) {
+    result.push(i.length)
+  }
+  return result
+}
+
+console.log(getMultipleLengths(["hello", "what", "is", "up", "dude"]))
+
+// G. maxOfThree------------------------------------------------------------------------------------------------------------
+function maxOfThree(num1, num2, num3) {
+  return Math.max(num1, num2, num3)
+}
+
+console.log(maxOfThree(6, 9, 1))
+
+// H. printLongestWord------------------------------------------------------------------------------------------------------------
+function printLongestWord(arrOfStrings) {
+  let longestWord = ""
+  for (let i = 0; i < arrOfStrings.length; i++) {
+    if (arrOfStrings[i].length > longestWord.length) {
+      longestWord = arrOfStrings[i]
+    }
+  }
+  return longestWord
+}
+
+console.log(
+  printLongestWord([
+    "BoJack",
+    "Princess",
+    "Diane",
+    "a",
+    "Max",
+    "Peanutbutter",
+    "big",
+    "Todd",
+  ])
+)
+
+// OBJECTS!!!!------------------------------------------------------------------------------------------------------------
+// A------------------------------------------------------------------------------------------------------------
+const user = {
+  name: "Caleb",
+  email: "caleb.burge@gmail.com",
+  age: 12,
+  purchased: [],
+}
+
+// B------------------------------------------------------------------------------------------------------------
+const otherUsr = { ...user }
+otherUsr.email = "newemail@gmail.com"
+otherUsr.age++
+
+// C------------------------------------------------------------------------------------------------------------
+otherUsr.location = "a location"
+console.log(otherUsr)
+
+// D------------------------------------------------------------------------------------------------------------
+user.purchased.push("carbohydrates")
+user.purchased.push("peace of mind")
+user.purchased.push("Merino jodhpurs")
+console.log(user.purchased[2])
+
+// E------------------------------------------------------------------------------------------------------------
+user.friend = {
+  name: "friend",
+  age: 17,
+  location: "friend location",
+  purchased: [],
+}
+console.log(user.friend.name)
+console.log(user.friend.location)
+user.friend.age = 55
+user.friend.purchased.push("The One Ring")
+user.friend.purchased.push("A latte")
+console.log(user.friend.purchased[1])
+
+// F------------------------------------------------------------------------------------------------------------
+for (item of user.purchased) {
+  console.log(item)
+}
+for (item of user.friend.purchased) {
+  console.log(item)
+}
+
+// G------------------------------------------------------------------------------------------------------------
+function updateUser() {
+  user.age++
+  user.name = user.name.toUpperCase()
+}
+console.log(user)
+
+function oldAndLoud(person) {
+  person.age++
+  person.name = person.name.toUpperCase()
+}
+oldAndLoud(user)
+console.log(user)
