4cc41c43acc803cbfc83e2a21d98c5e8e0c8ea76
cburgee <caleb.burge@gmail.com>
2023-07-09 18:29:17 -0500

    Wild Wild life complete

--- mod_1/total_recall/wk4d3.js
+++ mod_1/total_recall/wk4d3.js
@@ -2,38 +2,51 @@
 // Easy Going
 ////////////////////////////////
 
-for (let i = 0; i <= 20; i++) {
-  console.log(i)
-}
+// for (let i = 0; i <= 20; i++) {
+//   console.log(i)
+// }
 
 ////////////////////////////////
 // Get Even
 ////////////////////////////////
 
-for (let i = 0; i <= 200; i += 2) {
-  console.log(i)
-}
+// for (let i = 0; i <= 200; i += 2) {
+//   console.log(i)
+// }
 
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
 
-for (let i = 1; i < 101; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz")
-  } else if (i % 5 === 0) {
-    console.log("Buzz")
-  } else if (i % 3 === 0) {
-    console.log("Fizz")
-  } else {
-    console.log(i)
-  }
-}
+// for (let i = 1; i < 101; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz")
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz")
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz")
+//   } else {
+//     console.log(i)
+//   }
+// }
 
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
 
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant", 5000, "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"]
+plantee[2]++
+console.log(plantee[2])
+wolfy.splice(-1, 1, "Gotham City")
+console.log(wolfy)
+dart.splice(dart.length, 0, "Hawkins")
+console.log(dart)
+wolfy.splice(0, 1, "Gameboy")
+console.log(wolfy)
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
