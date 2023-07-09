5ad2c5e5be28c775f70fec264c264511e94b06d2
cburgee <caleb.burge@gmail.com>
2023-07-09 18:10:24 -0500

    finished FizzBuzz and removed redundant directory relating to the same question

--- mod_1/total_recall/wk4d3.js
+++ mod_1/total_recall/wk4d3.js
@@ -18,6 +18,18 @@ for (let i = 0; i <= 200; i += 2) {
 // Fizz Buzz
 ////////////////////////////////
 
+for (let i = 1; i < 101; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz")
+  } else if (i % 5 === 0) {
+    console.log("Buzz")
+  } else if (i % 3 === 0) {
+    console.log("Fizz")
+  } else {
+    console.log(i)
+  }
+}
+
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
