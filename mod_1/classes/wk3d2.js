686c8fffd0a32083f5361b3ee62af133c7d50ee9
cburgee <caleb.burge@gmail.com>
2023-07-10 13:32:40 -0500

    finished week 3 day 2 homework

---
+++ mod_1/classes/wk3d2.js
@@ -0,0 +1,44 @@
+class Cat {
+  constructor(name, color, weight, sex) {
+    this.name = name
+    this.color = color
+    this.weight = weight
+    this.sex = sex
+    this.getAttributes()
+    this.meow()
+  }
+
+  meow() {
+    console.log(`${this.name} *meows*`)
+  }
+  set changeName(newName) {
+    this.name = newName
+  }
+  getAttributes() {
+    const maleMsg = `This cat's name is: ${this.name}. His color is: ${this.color}, he wieghs: ${this.weight}lbs. and is male.`
+    const femaleMsg = `This cat's name is: ${this.name}. Her color is: ${this.color}, she wieghs: ${this.weight}lbs. and is female.`
+    if (this.#checkSex() === 1) {
+      console.log(maleMsg)
+    } else {
+      console.log(femaleMsg)
+    }
+  }
+
+  #checkSex() {
+    if (this.sex.toLowerCase() === "male") {
+      return 1
+    } else {
+      return 0
+    }
+  }
+}
+
+const cat1 = new Cat("Max", "Orange", 10, "male")
+const cat2 = new Cat("Bella", "Black", 8, "female")
+const cat3 = new Cat("Milo", "Gray", 11, "male")
+cat1.changeName = "Billy"
+cat2.changeName = "Lily"
+cat3.changeName = "Leo"
+console.log(cat1)
+console.log(cat2)
+console.log(cat3)
