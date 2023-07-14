78456bbbf5d2179cdac25ad1278b24324c3c8628
cburgee <caleb.burge@gmail.com>
2023-07-14 15:19:49 -0500

    created a secondary sub nav bar

--- mod_1/DOM_lab/script.js
+++ mod_1/DOM_lab/script.js
@@ -4,11 +4,13 @@ let menuLinks = [
   { text: "orders", href: "/orders" },
   { text: "account", href: "/account" },
 ]
-let main = document.querySelector("main")
-let docRoot = document.querySelector(":root")
-let mainBG = getComputedStyle(docRoot).getPropertyValue("--main-bg")
-let topMenuBG = getComputedStyle(docRoot).getPropertyValue("--top-menu-bg")
-let topMenu = document.getElementById("top-menu")
+const main = document.querySelector("main")
+const docRoot = document.querySelector(":root")
+const mainBG = getComputedStyle(docRoot).getPropertyValue("--main-bg")
+const topMenuBG = getComputedStyle(docRoot).getPropertyValue("--top-menu-bg")
+const subMenuBG = getComputedStyle(docRoot).getPropertyValue("--sub-menu-bg")
+const topMenu = document.getElementById("top-menu")
+const subMenu = document.getElementById("sub-menu")
 console.log(mainBG)
 main.style.backgroundColor = mainBG
 main.innerHTML = "<h1>SEI Rocks!</h1>"
@@ -22,3 +24,5 @@ menuLinks.forEach(function (obj) {
   newA.href = obj.href
   topMenu.appendChild(newA)
 })
+subMenu.style.height = "100%"
+subMenu.style.backgroundColor = subMenuBG
