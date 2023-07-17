60e44fbb97a375f7f3fee241bd1bf6d4465c496d
cburgee <caleb.burge@gmail.com>
2023-07-17 15:06:05 -0500

    started working on game's brain class and added necessary private methods

--- mod_1/trivia_quiz_game/script.js
+++ mod_1/trivia_quiz_game/script.js
@@ -7,6 +7,51 @@
 // Repeat Steps 1-6 for player 2
 // Compare final score for both players, lower score wins the round.
 // Plays best 2 out of 3
-// importing codes from module file
+// import questions variable from seperate folder so things are a little easier to keep track of.
 import questions from "./questions.js"
-console.log(questions)
+
+const questionsCopy = [...questions["results"]]
+console.log(questionsCopy)
+//Player class setup
+class Player {
+  constructor(name) {
+    this.name = name
+    this.score = 0
+  }
+}
+//Game Board class setup
+class Game {
+  constructor(setOfQuestions) {
+    this.isGameOn = true
+    this.questions = setOfQuestions
+    this.rightAnswers = 0
+  }
+
+  //custom implementation of a random number within range
+  #getRandomNumberInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+  //Grabs a single question out of the questions list copy
+  #getNextQuestion() {
+    const randNum = this.#getRandomNumberInRange(0, questionsCopy.length)
+    return this.questions[randNum]
+  }
+
+  //This is going to change
+  nextQuestion() {
+    const question = this.#getNextQuestion()
+    console.log(`
+    question: ${question.question}\n
+    incorrect_answers: ${question.incorrect_answers}\n
+    correct_answer: ${question.correct_answer}
+    `)
+  }
+}
+const gameBrain = new Game(questionsCopy)
+const player1 = new Player("Caleb")
+
+while (gameBrain.isGameOn === true && gameBrain.rightAnswers <= 10) {
+  gameBrain.nextQuestion()
+  console.log("loop ran")
+  gameBrain.isGameOn = false
+}
