bb0cfc2a57fe360fa9fa4913a7b4eb9cd0f68af0
cburgee <caleb.burge@gmail.com>
2023-08-24 13:50:19 -0500

    completed labs for the day

--- mod_3/express-paramaters-lab/server.js
+++ mod_3/express-paramaters-lab/server.js
@@ -3,29 +3,6 @@ const app = express()
 const PORT = 3000
 const HOSTNAME = "127.0.0.1"
 
-const eightBallResponses = [
-  "It is certain",
-  "It is decidedly so",
-  "Without a doubt",
-  "Yes definitely",
-  "You may rely on it",
-  "As I see it yes",
-  "Most likely",
-  "Outlook good",
-  "Yes",
-  "Signs point to yes",
-  "Reply hazy try again",
-  "Ask again later",
-  "Better not tell you now",
-  "Cannot predict now",
-  "Concentrate and ask again",
-  "Don't count on it",
-  "My reply is no",
-  "My sources say no",
-  "Outlook not so good",
-  "Very doubtful",
-]
-
 app.get("/", function (req, res) {
   res.send("Home Page")
 })
@@ -40,6 +17,29 @@ app.get("/tip/:total/:tipPercentage", function (req, res) {
 })
 
 app.get("/magic/:question", function (req, res) {
+  const eightBallResponses = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+  ]
+
   const question = req.params.question
   const randomNum = Math.floor(Math.random() * eightBallResponses.length)
   const randomResponse = eightBallResponses[randomNum]
